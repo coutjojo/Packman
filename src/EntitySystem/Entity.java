3afc76ebc6e9a3d2cb5d33a5c7ea0ed65f996809
Apfelkuch <spiele55337@freenet.de>
2020-08-10 14:27:53 +0200

    Grundgerüst movement

--- src/EntitySystem/Entity.java
+++ src/EntitySystem/Entity.java
@@ -3,7 +3,7 @@ package EntitySystem;
 import java.awt.*;
 
 public abstract class Entity {
-    private double posX,posY;
+    protected double posX,posY;
 
     public Entity(float posX, float posY){
         this.posX = posX;
--- src/EntitySystem/Player.java
+++ src/EntitySystem/Player.java
@@ -1,21 +1,47 @@
 package EntitySystem;
 
+import ImageLoad.Assets;
+import Input.Input;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Player extends Creature {
-    private int SPEED;
+    private double SPEED = 3.0f;
+
+    private double xMove;
+    private double yMove;
+
+    private BufferedImage texture;
 
     public Player(float posX, float posY) {
         super(posX, posY);
+        texture = Assets.packman;
     }
 
     @Override
     public void render(Graphics g) {
-
+        g.drawImage(texture,(int) posX,(int) posY,null);
     }
 
     @Override
     public void tick() {
-
+        this.move();
     }
+
+    public void move() {
+        posX += xMove;
+        posY += yMove;
+    }
+
+    public void getMove() {
+        if(Input.UP)
+            yMove = -SPEED;
+        if (Input.DOWN)
+            yMove = SPEED;
+        if (Input.RIGHT)
+            xMove = SPEED;
+        if (Input.LEFT)
+            xMove = -SPEED;
+    }
+
 }
