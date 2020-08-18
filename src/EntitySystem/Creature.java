91508e7e389e9aaae6259f06d021069b55503664
Apfelkuch <spiele55337@freenet.de>
2020-08-18 17:51:37 +0200

    Finish Collision

--- src/EntitySystem/Creature.java
+++ src/EntitySystem/Creature.java
@@ -3,7 +3,7 @@ package EntitySystem;
 import java.awt.*;
 
 public abstract class Creature extends Entity{
-    protected int width = 45, height = 45;
+    protected int width = 47, height = 47;
     public Creature(float posX, float posY, Rectangle collisionBOX){
         super( posX, posY, collisionBOX);
     }
--- src/EntitySystem/Player.java
+++ src/EntitySystem/Player.java
@@ -6,7 +6,6 @@ import Main.Handler;
 import Tiles.Tile;
 
 import java.awt.*;
-import java.awt.geom.Rectangle2D;
 
 public class Player extends Creature {
     private float SPEED = 3.0f;
@@ -16,11 +15,11 @@ public class Player extends Creature {
     private float xMoveOLD;
     private float yMoveOLD;
 
-    private final int lookingUP = 0;
-    private final int lookingRIGHT = 1;
-    private final int lookintDOWN = 2;
-    private final int lookintLEFT = 3;
-    private int lookingAT = lookingRIGHT;
+    private final int lookingRIGHT = 0;
+    private final int lookingLEFT = 1;
+    private final int lookingDOWN = 2;
+    private final int lookingUP = 3;
+    private int lookingAT;
 
     private Handler handler;
 
@@ -28,21 +27,23 @@ public class Player extends Creature {
         super(50,50,null);
         this.handler = handler;
         collisionBOX = new Rectangle(width,height);
+        lookingAT = lookingRIGHT; // starting view direction
     }
 
     @Override
     public void render(Graphics g) {
+        // paint packman with his viewing direction (lookingAT)
         switch (lookingAT) {
             case lookingRIGHT:
                 g.drawImage(Assets.packman_RIGHT,(int) posX,(int) posY,width,height,null);
                 break;
-            case lookintLEFT:
+            case lookingLEFT:
                 g.drawImage(Assets.packman_LEFT,(int) posX,(int) posY,width,height,null);
                 break;
             case lookingUP:
                 g.drawImage(Assets.packman_UP,(int) posX,(int) posY,width,height,null);
                 break;
-            case lookintDOWN:
+            case lookingDOWN:
                 g.drawImage(Assets.packman_DOWN,(int) posX,(int) posY,width,height,null);
                 break;
         }
@@ -55,57 +56,56 @@ public class Player extends Creature {
     }
 
     public void move() {
-        if (collide()) {
-            if (xMove == xMoveOLD)
+        if (collide()) { //
+            if (xMove == xMoveOLD) // canceling xMove, if collide
                 xMove = 0;
-            else
+            else // set xMove to xMoveOLD, if collision was not on the x-Axis
                 xMove = xMoveOLD;
 
-            if(yMove == yMoveOLD)
+            if(yMove == yMoveOLD) // canceling yMove, if collide
                 yMove = 0;
-            else
+            else // set yMove to yMoveOLD, if collision was not on the y-Axis
                 yMove = yMoveOLD;
-
-            xMoveOLD = 0;
-            yMoveOLD = 0;
         }
+
         // adjust the viewing direction
         if(xMove > 0)
             lookingAT = lookingRIGHT;
         if(xMove < 0)
-            lookingAT = lookintLEFT;
+            lookingAT = lookingLEFT;
         if(yMove > 0)
-            lookingAT = lookintDOWN;
+            lookingAT = lookingDOWN;
         if(yMove < 0)
             lookingAT = lookingUP;
 
+        // change the position with xMove and yMove
         posX += xMove;
         posY += yMove;
     }
 
     private boolean collide() {
-        if(xMove > 1) { // if collide when moving right
+        if(xMove > 0) { // if collide when moving right
             if((posX + width + SPEED) >= (handler.getGame().getGameState().getWorld().getWitdh() * Tile.TILEWIDTH)) // leaving on the right
                 return true;
             else if((handler.getGame().getGameState().getWorld().getTile((int)((posX + width + SPEED) / Tile.TILEWIDTH),   (int) ((posY) / Tile.TILEHEIGHT))).isSolid()) // if solid-Tile right-up
                 return true;
             else if((handler.getGame().getGameState().getWorld().getTile((int)((posX + width + SPEED) / Tile.TILEWIDTH),   (int) ((posY + height) / Tile.TILEHEIGHT))).isSolid()) // if solid-Tile right-up
                 return true;
-        } else if (xMove < -1) { // if collide when moving left
+        } else if (xMove < 0) { // if collide when moving left
             if(posX <= 0) // leaving on the left
                 return true;
             else if((handler.getGame().getGameState().getWorld().getTile((int)((posX - SPEED) / Tile.TILEWIDTH),           (int)((posY) / Tile.TILEHEIGHT))).isSolid()) // if solid-Tile left-up
                 return true;
             else if((handler.getGame().getGameState().getWorld().getTile((int)((posX - SPEED) / Tile.TILEWIDTH),           (int)((posY + height) / Tile.TILEHEIGHT))).isSolid()) // if solid-Tile left-down
                 return true;
-        } else if (yMove > 1) { // if collide when moving down
+        } else if (yMove > 0) { // if collide when moving down
             if((posY + height + SPEED) >= (handler.getGame().getGameState().getWorld().getHeight() * Tile.TILEHEIGHT)) // leaving on the bottom
                 return true;
             else if((handler.getGame().getGameState().getWorld().getTile((int)((posX) / Tile.TILEWIDTH),                   (int)((posY + height + SPEED) / Tile.TILEHEIGHT))).isSolid()) // if solid-Tile bottom-left
                 return true;
             else if((handler.getGame().getGameState().getWorld().getTile((int)((posX + width) / Tile.TILEWIDTH),           (int)((posY + height + SPEED) / Tile.TILEHEIGHT))).isSolid()) // if solid-Tile bottom-right
                 return true;
-        } else if (yMove < -1) { // if collide when moving up
+        } else if (yMove < 0) { // if collide when moving up
             if(posY <= 0) // leaving on the top
                 return true;
             else if((handler.getGame().getGameState().getWorld().getTile((int)((posX) / Tile.TILEWIDTH),                   (int)((posY - SPEED) / Tile.TILEHEIGHT))).isSolid()) // if solid-Tile top-left
@@ -117,22 +117,25 @@ public class Player extends Creature {
     }
 
     public void getMove() {
+        // set MoveOLD to the current Move
         yMoveOLD = yMove;
         xMoveOLD = xMove;
-        xMove = 0;
-        yMove = 0;
+        // set Move to the new Input and reset the other Move
         if(Input.UP) {
             yMove = -SPEED;
+            xMove = 0;
         }
         if (Input.DOWN) {
             yMove = SPEED;
+            xMove = 0;
         }
         if (Input.RIGHT) {
             xMove = SPEED;
+            yMove = 0;
         }
         if (Input.LEFT) {
             xMove = -SPEED;
+            yMove = 0;
         }
     }
-
 }
